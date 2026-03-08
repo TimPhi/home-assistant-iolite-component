5a18dc54d4a64c93e643984ed6477230350b0cf3
Tim Philipp <63869563+TimPhi@users.noreply.github.com>
2026-03-08 12:55:46 +0100

    Fix argument list formatting in init.py

--- custom_components/iolite/__init__.py
+++ custom_components/iolite/__init__.py
@@ -49,8 +49,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         password, 
         storage,
         scan_interval_seconds,
-        client_id
-        verify_ssl
+        client_id,
+        verify_ssl,
     )
 
     await coordinator.async_config_entry_first_refresh()
